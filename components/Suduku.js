dfe3c51f07a7f16adf674d2e1f6a81affa0b8c9e
Simon Yu <simon@micrometrics.com>
2018-09-01 00:55:34 -0400

    Fix stylings

--- components/Suduku.js
+++ components/Suduku.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react/native'
-import { StyleSheet, View, Text, ScrollView, Button} from 'react-native';
-import { Table, Rows } from 'react-native-table-component';
+import { StyleSheet, View, Text, ScrollView, Button, TouchableOpacity} from 'react-native';
+import { Table, Rows, Cell, TableWrapper} from 'react-native-table-component';
 
 @observer
 export default class Suduku extends React.Component {
@@ -33,6 +33,10 @@ export default class Suduku extends React.Component {
         });
     }
 
+    reset(e) {
+        this.props.store.reset()
+    }
+
     change(e) {
         this.setState({
            status: 'Solve'
@@ -43,21 +47,44 @@ export default class Suduku extends React.Component {
     render() {
         return (
                 <ScrollView contentContainerStyle={styles.container}>
-                    <View>
-                        <Text style={styles.header}>
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={ styles.header }>
                             Suduku
                         </Text>
                     </View>
                     <View>
-                        <Table borderStyle={styles.table}>
-                            <Rows data={this.props.store.data} textStyle={styles.text}/>
+                        <Table borderStyle={{borderWidth: 2, borderColor: '#add620'}}>
+                            {
+                                this.props.store.data.map((rowData, rowIndex) => {
+                                    return (
+                                        <TableWrapper key={rowIndex} style={styles.wrapper}>
+                                            {
+                                                ((rowData.map((cellData, cellIndex) => {
+                                                    return (<Cell key={cellIndex}
+                                                                  data={cellData === 0 ? '' : cellData}
+                                                                  style={cellData === 0 ? styles.empty : styles.solved}
+                                                                  textStyle={styles.text}/>)
+                                                })))
+                                            }
+                                        </TableWrapper>
+                                    )
+                                })
+                            }
+                            {/*<Rows data={this.props.store.data} textStyle={styles.text}/>*/}
                         </Table>
                     </View>
-                    <View style={{textAlign: 'center'}}>
-                        <Button title={ this.state.status }
-                                onPress={this.solve.bind(this)} />
-                        <Button title="Try Another"
-                                onPress={this.change.bind(this)} />
+                    <View style={{ alignItems: 'center' }}>
+                        <TouchableOpacity style={styles.btn} onPress={this.solve.bind(this)}>
+                            <Text style={styles.btnText}>{ this.state.status }</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btnDanger} onPress={this.reset.bind(this)}>
+                            <Text style={styles.btnText}>Reset</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ alignItems: 'center' }}>
+                        <TouchableOpacity style={styles.btn} onPress={this.change.bind(this)}>
+                            <Text style={styles.btnText}>Try Another</Text>
+                        </TouchableOpacity>
                     </View>
                 </ScrollView>
         );
@@ -67,25 +94,56 @@ export default class Suduku extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 15,
-        paddingTop: 15,
+        padding: 20,
         backgroundColor: '#fff',
-        justifyContent: 'space-evenly',
-        textAlign: 'center'
+        justifyContent: 'space-evenly'
     },
     header: {
-        fontFamily: 'Cochin',
         fontSize: 20,
+        color: '#add620',
         fontWeight: 'bold',
-        textAlign: 'center'
+        fontFamily: 'Cochin'
     },
-    table: {
-        width: 100,
-        borderWidth: 2,
-        borderColor: '#c8e1ff',
+    // table: {
+    //     width: 100,
+    //     borderWidth: 2,
+    //     borderColor: '#add620'
+    // },
+    wrapper: { flexDirection: 'row' },
+    solved: {
+        width: '11.2%',
+        backgroundColor: '#f0f0f0'
+    },
+    emply: {
+        width: '11.2%',
+        backgroundColor: '#fff'
     },
     text: {
         margin: 6,
-        textAlign: 'center'
+        textAlign: 'center',
+        fontFamily: 'Times'
+    },
+    btn: {
+        width: "80%",
+        height: 30,
+        marginBottom: 5,
+        backgroundColor: '#add620',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btnDanger: {
+        width: "80%",
+        height: 30,
+        marginBottom: 5,
+        backgroundColor: '#d60a11',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btnText: {
+        color: '#fff',
+        fontFamily: 'Times'
     }
+
 });
